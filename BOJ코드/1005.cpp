cc0fcd18ec02b46714d6977a34e7c250b98c2fc6
gomhyeok <gomhyeok@gmail.com>
2024-03-28 16:49:34 +0900

    위상정렬 필요

---
+++ BOJ코드/1005.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+int test_case;
+int n, k, w;
+int delay[1001];
+bool already[1001];
+vector<int> need[1001];
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin>>test_case;
+    cin>>n>>k;
+
+
+}
