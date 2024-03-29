817655c052650ab784bb5b7a213079a245048626
gomhyeok <gomhyeok@gmail.com>
2024-03-29 15:44:50 +0900

    1005.cpp ACM Craft -> 위상정렬을 통한 문제풀이

--- BOJ코드/1005.cpp
+++ BOJ코드/1005.cpp
@@ -4,14 +4,15 @@
 #include <stack>
 #include <queue>
 #include <cstring>
+#include <map>
 
 using namespace std;
 
 int test_case;
 int n, k, w;
-int delay[1001];
-bool already[1001];
-vector<int> need[1001];
+map<int, int> resultCost;
+map<int, int> Cost;
+int degree[1003];
 
 int main () {
     ios_base::sync_with_stdio(false);
@@ -19,7 +20,52 @@ int main () {
     cout.tie(0);
 
     cin>>test_case;
-    cin>>n>>k;
 
+    while(test_case > 0) {
+        int answer = 0;
+
+        cin>>n>>k;
+        vector<vector<int>> v(n+1);
+        queue<int>q;
+
+        for(int i=1; i<=n; i++) {
+            int cost;
+            cin>>cost;
+            Cost[i] = cost;
+            resultCost[i] = cost;
+        }
+
+        for(int i=0; i<k; i++) {
+            int x, y;
+            cin>>x>>y;
+            v[x].emplace_back(y);
+            degree[y]++;
+        }
+
+        cin>>w;
+
+        for(int i=1; i<=n; i++) {
+            if(i == w) continue;
+            else if(degree[i] == 0) q.emplace(i);
+        }
+        while(!q.empty()) {
+            int idx = q.front();
+            q.pop();
+            for(int i=0; i<v[idx].size(); i++) {
+                int now = v[idx][i];
+                resultCost[now] = max(resultCost[now], resultCost[idx] + Cost[now]);
+                if(--degree[now] == 0) q.emplace(now);
+            }
+        }
+
+        cout<<resultCost[w]<<"\n";
+
+        for(int i=1; i<=n; i++) {
+            degree[i] = 0;
+            resultCost[i] = 0;
+            Cost[i] = 0;
+        }
+        test_case--;
+    }
 
 }
