a4ce0fe6f4f0b2d05150645ab770fd8b95910acf
gomhyeok <gomhyeok@gmail.com>
2024-04-13 15:35:53 +0900

    1167.cpp 트리의 지름-> dfs

--- BOJ코드/1167.cpp
+++ BOJ코드/1167.cpp
@@ -7,10 +7,43 @@
 
 using namespace std;
 
+int v;
+int deep_node;
+int max_cost;
+vector<pair<int, int>> path[100001];
+
+void dfs(int cur, int prev, int cost) {
+    if(cost > max_cost) {
+        max_cost = cost;
+        deep_node = cur;
+    }
+
+    for(int i=0; i<path[cur].size(); i++) {
+        if(path[cur][i].first != prev) dfs(path[cur][i].first, cur, path[cur][i].second + cost);
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    int first, second, node;
 
-    
+    cin>>v;
+
+    for(int i=1; i<=v; i++) {
+        cin>>node;
+        cin>>first;
+        while(first != -1) {
+            cin>>second;
+            path[i].push_back({first, second});
+            cin>>first;
+        }
+    }
+
+    dfs(1, 0 ,0);
+    max_cost = 0;
+    dfs(deep_node, 0, 0);
+
+    cout<<max_cost<<endl;
 }
