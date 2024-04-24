5dfb41f06257a9521deead08f6715602a973deb2
gomhyeok <gomhyeok@gmail.com>
2024-04-24 16:22:35 +0900

    2096.cpp 내려가기

--- BOJ코드/2096.cpp
+++ BOJ코드/2096.cpp
@@ -1,47 +1,41 @@
 #include <iostream>
-#include <vector>
+#include <cstdio>
+#include <cstdlib>
 #include <algorithm>
+#include <vector>
+#include <cmath>
+#include <list>
 #include <queue>
 #include <stack>
-#define MAX_VAL 9999999
-
+#include <map>
+#include <ctime>
+#include <string.h>
+ 
 using namespace std;
+ 
+int main(void)
+{
+    int n, maxDP[3] = { 0,0,0 }, minDP[3] = { 0,0,0 }, input[3], temp_0, temp_2;
+    scanf("%d", &n);
+    scanf("%d %d %d", &maxDP[0], &maxDP[1], &maxDP[2]);
+    minDP[0] = maxDP[0]; minDP[1] = maxDP[1]; minDP[2] = maxDP[2];
+ 
+ 
+    for (int i = 1; i < n; i++) {
+        scanf("%d %d %d", &input[0], &input[1], &input[2]);
+        
+        temp_0 = maxDP[0]; temp_2 = maxDP[2];
+        maxDP[0] = max(maxDP[0], maxDP[1]) + input[0];
+        maxDP[2] = max(maxDP[1], maxDP[2]) + input[2];
+        maxDP[1] = max(max(temp_0, maxDP[1]), temp_2) + input[1];
 
-int n;
-int board[100000][3];
-int dp[100000][3][2];
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin>>n;
-
-    for(int i=0; i<n; i++) {
-        cin>>board[i][0]>>board[i][1]>>board[i][2];
-        dp[i][0][0] = MAX_VAL;
-        dp[i][1][0] = MAX_VAL;
-        dp[i][2][0] = MAX_VAL;
+        temp_0 = minDP[0]; temp_2 = minDP[2];
+        minDP[0] = min(minDP[0], minDP[1]) + input[0];
+        minDP[2] = min(minDP[1], minDP[2]) + input[2];
+        minDP[1] = min(min(temp_0, minDP[1]), temp_2) + input[1];
     }
-
-    dp[n-1][0][0] = board[n-1][0];
-    dp[n-1][0][1] = board[n-1][0];
-    dp[n-1][1][0] = board[n-1][1];
-    dp[n-1][1][1] = board[n-1][1];
-    dp[n-1][2][0] = board[n-1][2];
-    dp[n-1][2][1] = board[n-1][2];
-
-    for(int i=n-2; i>=0; i++) {
-        if(dp[i][0][0] > dp[i-1][0][0] + board[i][0]) dp[i][0][0] = dp[i-1][0][0] + board[i][0];
-        if(dp[i][0][0] > dp[i-1][1][0] + board[i][1]) dp[i][0][0] = dp[i-1][1][0] + board[i][1];
-        if(dp[i][1][0] > dp[i-1][0][0] + board[i][0]) dp[i][0][0] = dp[i-1][0][0] + board[i][0];
-        if(dp[i][1][0] > dp[i-1][1][0] + board[i][1]) dp[i][1][0] = dp[i-1][1][0] + board[i][1];
-        if(dp[i][1][0] > dp[i-1][2][0] + board[i][2]) dp[i][2][0] = dp[i-1][2][0] + board[i][2];
-        if(dp[i][2][0] > dp[i-1][1][0] + board[i][1]) dp[i][1][0] = dp[i-1][1][0] + board[i][1];
-        if(dp[i][2][0] > dp[i-1][2][0] + board[i][2]) dp[i][2][0] = dp[i-1][1][0] + board[i][1];
-    }
-    
-
-    
+ 
+    printf("%d ", max(max(maxDP[0], maxDP[1]), maxDP[2]));
+    printf("%d", min(min(minDP[0], minDP[1]), minDP[2]));
+    return 0;
 }
