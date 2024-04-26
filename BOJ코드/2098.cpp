2c2ece1dc47239f319aaf7c0166a6917f0c53981
gomhyeok <gomhyeok@gmail.com>
2024-04-26 17:35:26 +0900

    2098.cpp 외판원 순회 -> dp 와 비트마스킹을 이용한 풀이로 어떠한 조합의 여부를 확인할 때는 비트마스킹도 유용하다.

--- BOJ코드/2098.cpp
+++ BOJ코드/2098.cpp
@@ -3,13 +3,53 @@
 #include <stack>
 #include <algorithm>
 #include <queue>
+#include <cstring>
+#define INF 987654321
 
 using namespace std;
+
+int n;
+int board[16][16];
+int dp[16][1<<16];
+
+int dfs(int cur, int visited);
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
-    
+    cin>>n;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin>>board[i][j];
+        }
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    cout<<dfs(0, 1)<<endl;
+}
+
+int dfs(int cur, int visited) {
+    if(visited == (1<<n) - 1) {
+        if(board[cur][0] == 0) {
+            return INF;
+        }
+        return board[cur][0];
+    }
+
+    if(dp[cur][visited] != -1) return dp[cur][visited];
+
+    dp[cur][visited] = INF;
+
+    for(int i=0; i<n; i++) {
+        if(board[cur][i] == 0) continue;
+        if((visited&(1<<i)) == (1<<i)) continue;
+
+        dp[cur][visited] = min(dp[cur][visited], board[cur][i] + dfs(i, visited | (1<<i)));
+    }
+
+    return dp[cur][visited];
 }
