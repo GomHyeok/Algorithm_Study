8d66ebe6a724a24b344fe581e1a77be42561dbfa
gomhyeok <gomhyeok@gmail.com>
2024-05-12 18:17:04 +0900

    1052.cpp 물병

---
+++ BOJ코드/1052.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+}
