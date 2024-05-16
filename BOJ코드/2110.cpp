92a1274887b597eb7d9d55c216ee447b7e5a0f65
gomhyeok <gomhyeok@gmail.com>
2024-05-16 14:32:49 +0900

    2110.cpp 공유기 설치 -> 집이 아니라 거리를 일렬로 두고 그 값을 검사함

--- BOJ코드/2110.cpp
+++ BOJ코드/2110.cpp
@@ -29,22 +29,20 @@ int main() {
     int maxDist = house[n-1] - house[0];
 
     while(minDist <= maxDist) {
-        int midDist = (minDist + maxDist)/2;
+        int midDist = (minDist + maxDist)/2;//최대 거리가 될 수 있는 값
 
-        //cout<<minDist<<" "<<maxDist<<" "<<midDist<<endl;
-
-        int cnt = 1;
-        int prev = house[0];
+        int cnt = 1;//공유기 설치 개수
+        int prev = house[0];//첫번째 집에는 무조건 설치되어야함 + 현재 위치
         for(int i=1; i<n; i++) {
-            if(house[i] - prev >= midDist) {
+            if(house[i] - prev >= midDist) {//최대 거리 이상 벌어진 집에 설치
                 cnt++;
-                prev = house[i];
+                prev = house[i];//기준 집 위치 변경
             }
         }
 
-        if(cnt >= c) {
+        if(cnt >= c) {//개수 충분히 설치한 경우
             answer = max(answer, midDist);
-            minDist = midDist + 1;
+            minDist = midDist + 1;//최소값을 변경
         }
         else maxDist = midDist -1;
     }
