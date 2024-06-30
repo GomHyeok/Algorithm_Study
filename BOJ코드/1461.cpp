e994ff6157bf4617a0eed985dc2f962ecb3c5c07
gomhyeok <gomhyeok@gmail.com>
2024-06-30 19:21:59 +0900

    1461.cpp 도서관 -> 단순 정렬 이후에 한번에 갈수 있는 거리를 계산

---
+++ BOJ코드/1461.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int pos;
+int minusBook;
+int answer;
+vector<int> books;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++) {
+        int a;
+        cin>>a;
+        if(a < 0) {
+            minusBook++;
+        }
+        books.push_back(a);
+    }
+
+    sort(books.begin(), books.end());
+
+    for(int i=n-1; i>=minusBook; i-=m) {
+        answer += (books[i] * 2); // 양수인 책들
+    }
+
+    for(int i=0; i<minusBook; i+= m) {
+        answer += abs(books[i] * 2); // 음수인 책들
+    }
+
+    answer -= max(abs(books[0]), abs(books[n-1]));
+
+    cout<<answer<<endl;
+
+    return 0;
+}
